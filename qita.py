62def0e3b72ea0302c92fcaa33cb101d5540c45c
PaiDX1314 <253373473@qq.com>
2024-02-28 01:28:54 +0800

    Add files via upload

--- qita.py
+++ qita.py
@@ -23,7 +23,7 @@ with open("itv.txt", 'r', encoding='utf-8') as file:
         line = line.strip()
         if line:
             channel_name, channel_url = line.split(',')
-            if '卫视' not in channel_name and 'CCTV' not in channel_name and 'CGTN' not in channel_name and 'CETV' not in channel_name:
+            if '卫视' not in channel_name and 'CCTV' not in channel_name and '测试' not in channel_name:
                 channels.append((channel_name, channel_url))
 
 # 定义工作线程函数
@@ -100,14 +100,14 @@ results.sort(key=lambda x: (x[0], -float(x[2].split()[0])))
 now_today = datetime.date.today()
 # 将结果写入文件
 
-result_counter = 5  # 每个频道需要的个数
+result_counter = 8  # 每个频道需要的个数
 
 with open("qita.txt", 'w', encoding='utf-8') as file:
     channel_counters = {}
     file.write('其他频道,#genre#\n')
     for result in results:
         channel_name, channel_url, speed = result
-        if '卫视' not in channel_name and 'CCTV' not in channel_name and 'CGTN' not in channel_name and 'CETV' not in channel_name:
+        if 'CCTV' not in channel_name and '卫视' not in channel_name and '测试' not in channel_name:
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
                     continue
@@ -125,7 +125,7 @@ with open("qita.m3u", 'w', encoding='utf-8') as file:
     #file.write('其他频道,#genre#\n')
     for result in results:
         channel_name, channel_url, speed = result
-        if '卫视' not in channel_name and 'CCTV' not in channel_name and 'CGTN' not in channel_name and 'CETV' not in channel_name:
+        if 'CCTV' not in channel_name and '卫视' not in channel_name and '测试' not in channel_name:
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
                     continue
